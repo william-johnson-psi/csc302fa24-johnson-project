76bcfbb723fd602033e1f24a6429571b031ccae7
william-johnson-psi <william.johnson.psi@gmail.com>
2024-11-21 21:50:47 -0500

    feat: logged in user displays on all pages

--- src/js/login-logic.js
+++ src/js/login-logic.js
@@ -33,45 +33,6 @@ $(document).ready(function() {
 })
 
 /**
- * Grab the signed in user info, and pass it to the functions given  
- * 
- * Each object passed will have a 'success:boolean', use this to decide what to do with the received data.
- * 
- *      false indicates that the user is not signed in or there was an issue grabbing that information
- *      true indicates that the user is signed in, and you will have access to the information from the API
- * 
- * {data} will be passed to each given function, so be aware of this when creating a new function or lambda.
- * 
- * @param {function} dataSuccessFunction 
- * @param {function} dataErrorFunction 
- * @param {function} errorFunction 
- */
-function getSignedInUserInfo(dataSuccessFunction, dataErrorFunction, errorFunction) {
-    $.ajax({
-        url: '../php/api.php',
-        method: 'POST',
-        data: {'action':'get-signin'},
-        dataType: 'json',
-        success: function(data) {
-            if (data.success) {
-                console.log('Grabbed User Info');
-                dataSuccessFunction(data);
-            }
-            else {
-                console.log(data.error);
-                dataErrorFunction(data);
-            }
-        },
-        error: function(jqXHR, status, error) {
-            console.log('Error getting sign in information: ' + error);
-            errorFunction({success:false});
-            console.log(jqXHR);
-        },
-    })    
-}
-
-
-/**
  * In the Login Info page, *IF* the user is signed in
  * 1. Hide the sign-in and sign-up containers 
  * 2. Display the sign-out button
